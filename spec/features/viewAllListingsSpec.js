fae8323baad196e8160c2d4293d79c2b50815f90
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-11 11:25:13 +0100

    write most basic visit / and expect Hello World feature test

--- spec/features/viewAllListingsSpec.js
+++ spec/features/viewAllListingsSpec.js
@@ -1,17 +1,27 @@
-// 'use strict'
-//
-//
-// describe('View all listings features', function() {
-//
-//   beforeEach(function) {
-//     var list = new List();
-//   };
-//
-//
-//   describe('Homepage', function() {
-//     it("returns list of all properties", function () {
-//
-//     });
-//   });
-//
-// });
+'use strict'
+
+describe('View all listings features', function() {
+
+  // beforeEach(function () {
+  //   var list = new List();
+  // })
+
+  describe('/', function() {
+    it('responds with string: Hello World!, function () {
+      visit('/')
+      // expect(page).to have_content 'Hello World!'
+      page.should have_content 'Hello World!'
+    })
+  })
+
+  // describe('Homepage', function() {
+  //   it('displays list of all properties', function () {
+  //     visit('/homepage')
+  //     expect(page).to have_content('')
+  //     // page.should have_content 'title'
+  //   })
+  // })
+
+
+
+});
